f93339c18d66d2c9bb22dea035412a8d6027fdfb
ssindra <ssindra25@gmail.com>
2017-06-15 03:17:46 +0800

    progres login

--- js/components/login/index.js
+++ js/components/login/index.js
@@ -1,124 +1,91 @@
-import React, { Component } from "react";
-import { Image } from "react-native";
-import { connect } from "react-redux";
-import {
-  Container,
-  Content,
-  Item,
-  Input,
-  Button,
-  Icon,
-  View,
-  Text
-} from "native-base";
-import { Actions } from "react-native-router-flux";
-import { Field, reduxForm } from "redux-form";
-import { setUser } from "../../actions/user";
-import styles from "./styles";
+import React, { Component, PropTypes } from 'react';
+import { Image, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { Container, Content, Item, Input, Button, Icon, View, Text } from 'native-base';
+import { Actions } from 'react-native-router-flux';
 
-const background = require("../../../images/logo2.png");
+import { setUser } from '../../actions/user';
+import styles from './styles';
 
-const validate = values => {
-  const error = {};
-  error.email = "";
-  error.password = "";
-  var ema = values.email;
-  var pw = values.password;
-  if (values.email === undefined) {
-    ema = "";
-  }
-  if (values.password === undefined) {
-    pw = "";
-  }
-  if (ema.length < 8 && ema !== "") {
-    error.email = "too short";
-  }
-  if (!ema.includes("@") && ema !== "") {
-    error.email = "@ not included";
-  }
-  if (pw.length > 12) {
-    error.password = "max 11 characters";
-  }
-  if (pw.length < 5 && pw.length > 0) {
-    error.password = "Weak";
-  }
-  return error;
-};
+var SERVER_LOGIN_URL = 'http://mhs.rey1024.com/1415051094/index.php';
+
+const background = require('../../../images/shadow.png');
 
 class Login extends Component {
-  static propTypes = {
-    setUser: React.PropTypes.func
-  };
+
+  // static propTypes = {
+  //   setUser: React.PropTypes.func,
+  // }
+
   constructor(props) {
     super(props);
     this.state = {
-      name: ""
+      username: "",
+      password: "",
     };
-    this.renderInput = this.renderInput.bind(this);
   }
 
-  setUser(name) {
-    this.props.setUser(name);
+  // setUser(name) {
+  //   this.props.setUser(name);
+  // }
+
+  login() {
+    fetch(SERVER_LOGIN_URL + '?username=' + this.state.username + '&password=' + this.state.password)
+      .then((response) => response.json())
+      .then((responseData) => {
+        var id = responseData.id;
+        if (id === -1) {
+          Alert.alert("Wrong Username or Password!")
+         }
+         else 
+       {
+        Actions.home();
+        }  
+        
+      })
+      .done();
   }
-  renderInput({
-    input,
-    label,
-    type,
-    meta: { touched, error, warning },
-    inputProps
-  }) {
-    var hasError = false;
-    if (error !== undefined) {
-      hasError = true;
-    }
-    return (
-      <Item error={hasError}>
-        <Icon active name={input.name === "email" ? "person" : "unlock"} />
-        <Input
-          placeholder={input.name === "email" ? "EMAIL" : "PASSWORD"}
-          {...input}
-        />
-        {hasError
-          ? <Item style={{ borderColor: "transparent" }}>
-              <Icon active style={{ color: "red", marginTop: 5 }} name="bug" />
-              <Text style={{ fontSize: 15, color: "red" }}>{error}</Text>
-            </Item>
-          : <Text />}
-      </Item>
-    );
-  }
+
   render() {
     return (
       <Container>
         <View style={styles.container}>
           <Content>
-            <Image source={background} style={styles.shadow}>
+            <Image source={background} style={styles.shadow}/>
               <View style={styles.bg}>
-                <Field name="email" component={this.renderInput} />
-                <Field name="password" component={this.renderInput} />
-                <Button style={styles.btn} onPress={() => Actions.home()}>
+                <Item style={styles.input}>
+                  <Icon active name="person" />
+                  <Input 
+                    placeholder="USERNAME" 
+                    text = {this.state.username}
+                    onChangeText={(e) => this.setState({ username: e })} 
+                  />
+                </Item>
+                <Item style={styles.input}>
+                  <Icon name="unlock" />
+                  <Input
+                    placeholder="PASSWORD"
+                    secureTextEntry
+                    text = {this.state.password}
+                    onChangeText={(e) => this.setState({ password: e })}
+                  />
+                </Item>
+                <Button style={styles.btn} onPress={() => this.login()}>
                   <Text>Login</Text>
                 </Button>
               </View>
-            </Image>
           </Content>
         </View>
       </Container>
     );
   }
 }
-const LoginSwag = reduxForm(
-  {
-    form: "test",
-    validate
-  },
-  function bindActions(dispatch) {
-    return {
-      setUser: name => dispatch(setUser(name))
-    };
-  }
-)(Login);
-LoginSwag.navigationOptions = {
-  header: null
-};
-export default LoginSwag;
+
+function bindActions(dispatch) {
+  return {
+    setUser: email => dispatch(setUser(email)),
+  };
+}
+
+
+export default connect(null, bindActions)(Login);
